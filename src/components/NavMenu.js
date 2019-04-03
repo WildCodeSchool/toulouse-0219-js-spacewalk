f3a5d135b9f9141843ff8b4418b533aef4ec4c51
Dens <grosdenis@gmail.com>
2019-04-03 18:50:43 +0200

    Navbar structure finished

--- src/components/NavMenu.js
+++ src/components/NavMenu.js
@@ -12,7 +12,7 @@ import {
 } from 'reactstrap';
 
 const searchStyle = {
-  color: 'red'
+  color: 'var(--primary-color)'
 };
 
 class NavMenu extends Component {
@@ -40,19 +40,23 @@ class NavMenu extends Component {
           <NavbarBrand href="/">SpaceApp</NavbarBrand>
           <NavbarToggler onClick={this.toggle} className="navToggler" />
           <Collapse className="navCollapse" isOpen={isOpen} navbar>
-            <Nav className="ml-auto" navbar>
-              <NavItem>
-                <NavLink href="/">Home</NavLink>
+            <Nav className="nav" navbar>
+              <NavItem className="navItem flex-grow-1">
+                <NavLink className="text-center mr-4" href="/">Home</NavLink>
               </NavItem>
-              <NavItem>
-                <NavLink href="/articles">Articles</NavLink>
+              <NavItem className="navItem flex-grow-1">
+                <NavLink className="text-center mr-4" href="/articles">Articles</NavLink>
               </NavItem>
-              <NavItem>
-                <NavLink href="/read">To read later</NavLink>
+              <NavItem className="navItem flex-grow-1">
+                <NavLink className="text-center mr-4" href="/read">To read later</NavLink>
+              </NavItem>
+              <NavItem className="navItem">
+                <Input className="input" style={searchStyle} type="text" placeholder="Search" />
+              </NavItem>
+              <NavItem className="navItem">
+                <Button block className="btn" variant="outline-success">Search</Button>
               </NavItem>
             </Nav>
-            <Input style={searchStyle} type="text" placeholder="Search" />
-            <Button variant="outline-success">Search</Button>
           </Collapse>
         </Navbar>
       </div>
