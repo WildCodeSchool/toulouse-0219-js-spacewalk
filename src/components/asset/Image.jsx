616491805af5f5373479bd576bcc969a416f77a4
Sandy Prolhac <tottati@hotmail.com>
2019-04-10 21:46:39 +0200

    video share ajouté mais non fonctionnel

--- src/components/asset/Image.jsx
+++ src/components/asset/Image.jsx
@@ -1,6 +1,25 @@
+import {
+  FacebookShareButton,
+  LinkedinShareButton,
+  TwitterShareButton,
+  WhatsappShareButton,
+  PinterestShareButton,
+  RedditShareButton,
+  TumblrShareButton,
+  FacebookIcon,
+  TwitterIcon,
+  WhatsappIcon,
+  LinkedinIcon,
+  PinterestIcon,
+  RedditIcon,
+  TumblrIcon,
+} from 'react-share';
+
 import PropTypes from 'prop-types';
 import React from 'react';
 import Title from '../title';
+
+
 // Affichage de la page de l'image avec les détails
 const Image = ({ description, href, title, date }) => (
   <div clasName="container">
@@ -9,6 +28,43 @@ const Image = ({ description, href, title, date }) => (
         <Title title={title} />
         <p>{date}</p>
         <p>{description}</p>
+        <div className="row text-center mx-auto d-flex justify-content-center">
+          <div>
+            <FacebookShareButton url={href}>
+              <FacebookIcon size={32} round />
+            </FacebookShareButton>
+          </div>
+          <div>
+            <TwitterShareButton url={href} title={title}>
+              <TwitterIcon size={32} round />
+            </TwitterShareButton>
+          </div>
+          <div>
+            <LinkedinShareButton url={href} title={title}>
+              <LinkedinIcon size={32} round />
+            </LinkedinShareButton>
+          </div>
+          <div>
+            <WhatsappShareButton url={href} title={title}>
+              <WhatsappIcon size={32} round />
+            </WhatsappShareButton>
+          </div>
+          <div>
+            <PinterestShareButton url={href} media={href} description={title}>
+              <PinterestIcon size={32} round />
+            </PinterestShareButton>
+          </div>
+          <div>
+            <RedditShareButton url={href} title={title}>
+              <RedditIcon size={32} round />
+            </RedditShareButton>
+          </div>
+          <div>
+            <TumblrShareButton url={href} title={title}>
+              <TumblrIcon size={32} round />
+            </TumblrShareButton>
+          </div>
+        </div>
         <img className="shadow p-3 mb-5 bg-white rounded" alt={title} src={href} />
       </div>
     </div>
--- src/components/asset/Video.jsx
+++ src/components/asset/Video.jsx
@@ -1,13 +1,68 @@
+import {
+  FacebookShareButton,
+  LinkedinShareButton,
+  TwitterShareButton,
+  WhatsappShareButton,
+  PinterestShareButton,
+  RedditShareButton,
+  TumblrShareButton,
+  FacebookIcon,
+  TwitterIcon,
+  WhatsappIcon,
+  LinkedinIcon,
+  PinterestIcon,
+  RedditIcon,
+  TumblrIcon,
+} from 'react-share';
+
+
 import PropTypes from 'prop-types';
 import React from 'react';
 import { Player } from 'video-react';
 import Title from '../title';
 // Affichage de la page de la vidéo avec les détails et le player
-const Video = ({ description, title, id }) => (
+const Video = ({ description, title, id, href }) => (
   <div className="container">
-    <div className="asset-video">
+    <div className="mb-5">
       <Title title={title} />
       <p>{description}</p>
+      <div className="row text-center mx-auto d-flex justify-content-center mb-3">
+        <div>
+          <FacebookShareButton url={`http://images-assets.nasa.gov/video/${id}/${id}~medium.mp4`}>
+            <FacebookIcon size={32} round />
+          </FacebookShareButton>
+        </div>
+        <div>
+          <TwitterShareButton url={`http://images-assets.nasa.gov/video/${id}/${id}~medium.mp4`} title={title}>
+            <TwitterIcon size={32} round />
+          </TwitterShareButton>
+        </div>
+        <div>
+          <LinkedinShareButton url={`http://images-assets.nasa.gov/video/${id}/${id}~medium.mp4`} title={title}>
+            <LinkedinIcon size={32} round />
+          </LinkedinShareButton>
+        </div>
+        <div>
+          <WhatsappShareButton url={`http://images-assets.nasa.gov/video/${id}/${id}~medium.mp4`} title={title}>
+            <WhatsappIcon size={32} round />
+          </WhatsappShareButton>
+        </div>
+        <div>
+          <PinterestShareButton url={`http://images-assets.nasa.gov/video/${id}/${id}~medium.mp4`} media={href} description={title}>
+            <PinterestIcon size={32} round />
+          </PinterestShareButton>
+        </div>
+        <div>
+          <RedditShareButton url={`http://images-assets.nasa.gov/video/${id}/${id}~medium.mp4`} title={title}>
+            <RedditIcon size={32} round />
+          </RedditShareButton>
+        </div>
+        <div>
+          <TumblrShareButton url={`http://images-assets.nasa.gov/video/${id}/${id}~medium.mp4`} title={title}>
+            <TumblrIcon size={32} round />
+          </TumblrShareButton>
+        </div>
+      </div>
       <Player
         playsInline
         src={`http://images-assets.nasa.gov/video/${id}/${id}~medium.mp4`}
