c65dc4d24bb58eb3bee9cea2e7b89740c6c24745
Dens <grosdenis@gmail.com>
2019-04-23 20:36:25 +0200

    Filtered pagination done

--- src/components/CollectionAndResult.js
+++ src/components/CollectionAndResult.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import Collections from './collections';
 import { Badge, Button } from 'reactstrap';
-import HomePagination from './HomePagination';
 import './collections.css';
 import Title from './title';
 import './articleHome.css';
@@ -14,6 +13,14 @@ import photo from './images/photoCollection.jpeg';
 import hubble from './images/hubbleCollection.png';
 import exoplanet from './images/exoplanetCollection.jpeg';
 import excerptDate from '../functions/excerptDate';
+
+const pageNbrStyle = {
+  display: "flex",
+  color: "blue",
+  userSelect: "none",
+  cursor: "pointer",
+  listStyleType: "none"
+}
 
 class CollectionAndResult extends Component {
   constructor(props) {
@@ -39,11 +46,12 @@ class CollectionAndResult extends Component {
           url: exoplanet,
         }
       ],
-      pageOfArticles: []
+      currentPage: 1,
+      articlesPerPage: 6
     };
 
     // Binding method
-    this.onChangePage = this.onChangePage.bind(this);
+    this.handleClick = this.handleClick.bind(this);
   }
 
   componentDidMount() {
@@ -59,20 +67,52 @@ class CollectionAndResult extends Component {
       });
     this.handleTag = this.handleTag.bind(this);
   }
-  handleTag = tag => this.setState({ tagName: tag });
+  handleTag = tag => this.setState({
+    tagName: tag,
+    currentPage: 1
+  });
 
-  onChangePage(newSetOfArticles) {
-    // Updating page with new page of articles
-    this.setState({ pageOfArticles: newSetOfArticles });
+  handleClick(event) {
+    this.setState({ currentPage: Number(event.target.value) });
   }
 
   render() {
-    const filteredArticleList = this.state.article
+    // Destructuring
+    const { article, currentPage, articlesPerPage } = this.state;
+
+    // Woking with filtered articles in order to display a reliable number of pages
+    const filteredArticles = article
       .filter(
         singleArt =>
           this.state.tagName === "" ||
           singleArt.title.includes(this.state.tagName)
       );
+
+    // Logic for displaying articles
+    const indexOfLastArticle = currentPage * articlesPerPage;
+    const indexOfFirstArticle = indexOfLastArticle - articlesPerPage;
+    const currentArticles = filteredArticles.slice(indexOfFirstArticle, indexOfLastArticle);
+
+    // Logic for displaying page numbers
+    const pageNumbers = [];
+    for (let i = 1; i <= Math.ceil(filteredArticles.length / articlesPerPage); i += 1) {
+      pageNumbers.push(i);
+    }
+
+    // Display pages
+    const renderPageNumbers = pageNumbers.map(number => {
+      return (
+        <li
+          key={number}
+          value={number}
+          onClick={this.handleClick}
+        >
+          {number}
+        </li>
+      );
+    });
+
+
 
     return (
       <section>
@@ -94,54 +134,54 @@ class CollectionAndResult extends Component {
             ))}
           </div>
           <div>
-            <HomePagination articleList={filteredArticleList} onChangePage={this.onChangePage} />
+            <ul style={pageNbrStyle}>{renderPageNumbers}</ul>
           </div>
           <div className="row mx-auto">
-            {this.state.article
+            {/* {currentArticles
               .filter(
                 singleArt =>
                   this.state.tagName === "" ||
                   singleArt.title.includes(this.state.tagName)
-              )
-              .map((singleArt, index) => (
+              ) */}
+            {currentArticles.map((singleArt, index) => (
 
-                <div key={index} className="articleHome">
-                  <img
-                    src={singleArt.image}
-                    alt={singleArt.title}
-                  />
-                  <div>
-                    {this.state.tag
-                      .filter(SingleTag => singleArt.title.includes(SingleTag.name))
-                      .map(SingleTag => (
-                        <p
-                          style={{
-                            backgroundColor: colorTag(SingleTag.name),
-                            color: "#ffffff"
-                          }}
-                          type="button"
-                          className="badge mr-1"
-                          onClick={() => this.handleTag(SingleTag.name)}
-                        >{SingleTag.name}</p>
-                      ))}
-                    <p className="date">
-                      {excerptDate(singleArt.pub_date)}
-                    </p>
-                    <h2>{singleArt.title}</h2>
-                    {/* <p>
+              <div key={index} className="articleHome">
+                <img
+                  src={singleArt.image}
+                  alt={singleArt.title}
+                />
+                <div>
+                  {this.state.tag
+                    .filter(SingleTag => singleArt.title.includes(SingleTag.name))
+                    .map(SingleTag => (
+                      <p
+                        style={{
+                          backgroundColor: colorTag(SingleTag.name),
+                          color: "#ffffff"
+                        }}
+                        type="button"
+                        className="badge mr-1"
+                        onClick={() => this.handleTag(SingleTag.name)}
+                      >{SingleTag.name}</p>
+                    ))}
+                  <p className="date">
+                    {excerptDate(singleArt.pub_date)}
+                  </p>
+                  <h2>{singleArt.title}</h2>
+                  {/* <p>
                       {excerpt(singleArt.description, 10)}
                     </p> */}
-                    <a href={singleArt.link}>
-                      <Button
-                        color="dark"
-                        className="btn-sm"
-                        outline>
-                        Read more ⇢
+                  <a href={singleArt.link}>
+                    <Button
+                      color="dark"
+                      className="btn-sm"
+                      outline>
+                      Read more ⇢
                       </Button>
-                    </a>
-                  </div>
+                  </a>
                 </div>
-              ))}
+              </div>
+            ))}
           </div>
 
         </div>
